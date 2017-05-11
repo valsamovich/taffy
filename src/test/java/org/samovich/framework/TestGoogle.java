0fa658dd4c226d5a222b0c7ab1c1a0922c212af7
Valery Samovich <valery.y.samovich@disney.com>
2017-05-11 17:48:12 -0400

    Update test

--- src/test/java/org/samovich/framework/TestGoogle.java
+++ src/test/java/org/samovich/framework/TestGoogle.java
@@ -1,14 +1,11 @@
 package org.samovich.framework;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -17,14 +14,14 @@ import static org.junit.Assert.fail;
 /**
 /**
  * @author Valery Samovich
- * @see
+ * @see WebDriver
+ * @see StringBuffer
  */
 public class TestGoogle {
 
-    private static final String CHROME_DRIVER_PATH = "../chromedriver";
+    private static final String CHROME_DRIVER_PATH = "./chromedriver";
     private static final String BASE_URL = "https://google.com/";
     private WebDriver driver;
-    private WebDriverWait wait;
     private StringBuffer verificationErrors = new StringBuffer();
 
     @Before
@@ -38,16 +35,7 @@ public class TestGoogle {
     @Test
     public void testExample() throws Exception {
         driver.get(BASE_URL);
-        wait = new WebDriverWait(driver, 20);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputEmail")));
-        driver.findElement(By.id("inputEmail")).sendKeys("...");
-        driver.findElement(By.id("inputPassword")).sendKeys("...!");
-        driver.findElement(By.id("submitButton")).click();
-        driver.findElement(By.xpath("//*[@id=\"submenu\"]/div/ul/li[3]/a")).click();
-        driver.findElement(By.xpath("//*[@id=\"submenu\"]/div/ul/li[3]/ul/li[2]/a")).click();
-        driver.findElement(By.xpath("//*[@id=\"dataTable\"]/tbody/tr[23]/td[7]/a")).click();
-        String revenueAsStars = driver.findElement(By.xpath("//*[@id=\"dataTable\"]/tbody/tr[10]/td[2]")).getText();
-        Assert.assertTrue("Revenue have only stars!", revenueAsStars.contains("***"));
+        driver.findElement(By.id("hplogo"));
     }
 
     @After
